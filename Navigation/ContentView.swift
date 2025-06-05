2af61a8d41d4eb4dc057511cadcc55d1ec141e74
Evie <Samantha.Fisher123@gmail.com>
2025-06-05 10:05:00 -0500

    added a second view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("This is the root view😎")
+                NavigationLink(destination: Text("You've arrived to the Second View 🎊") ) {
+                    Text("Click me!")
+                        .font(.title3)
+                }
+                
+            }
+            //end VStack
         }
-        .padding()
+        //end NavSatck
     }
+    //end body
 }
-
+//end struct
 #Preview {
     ContentView()
 }
